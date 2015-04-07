ef3c1acabb8719aa1bf85505f9b7e21fb28062b6
Jussi Hallila <jussi@hallila.com>
2015-04-07 19:54:24 +0100

    Started building up the test suite.

---
+++ spec/components/hello/helloStore.spec.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const expect = require('chai').expect;
+const helloStore = require('../../../app/components/hello/HelloStore');
+const Dispatcher = require('../../../app/util/dispatcher');
+
+describe('HelloStore', function () {
+  it('should return default values of Loading and firstRun true', function () {
+    const response = helloStore.getData();
+    expect(response).to.have.property('text', 'Loading... ');
+    expect(response.metadata).to.have.property('firstRun', true);
+  });
+  describe('Listener Testing', function () {
+    const helloGet = require('../../../app/components/hello/HelloConstants').HelloConstants.GET;
+    let spy = {called: false};
+
+    function spyFunction() {
+      spy.called = true;
+    }
+
+    helloStore.addChangeListener(helloGet, spyFunction);
+    it('should emit an event when new data is set', function () {
+      let dispatch = Dispatcher.transmit(helloGet);
+      dispatch();
+      expect(spy).to.have.property('called', true);
+    });
+    it('should not emit an event when listener is removed', function () {
+      spy.called = false;
+      helloStore.removeChangeListener(helloGet, spyFunction);
+      let dispatch = Dispatcher.transmit(helloGet);
+      dispatch();
+      expect(spy).to.have.property('called', false);
+    });
+  });
+
+});
