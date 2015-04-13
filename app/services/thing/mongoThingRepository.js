9a76fa7649e9337e402bb015a79ad12623e7d967
Jussi Hallila <jussi@hallila.com>
2015-04-13 17:04:32 +0100

    Modified queries. Response objects need to be formatted. Other than that this should be done.

--- app/services/thing/mongoThingRepository.js
+++ app/services/thing/mongoThingRepository.js
@@ -2,29 +2,36 @@
 
 export function getThingsById(db, id, cb) {
   const User = db.model('User');
-  return User.findById(id, function (err, user) {
-    if (err) {
-      return cb(err, null);
+  return User.findById(id, function (userErr, user) {
+    if (userErr) {
+      return cb(userErr, null);
     }
-    return cb(null, user.things);
+    db.model('Thing').find({
+      '_id': {$in: user.things}
+    }, function (err, things) {
+      if (err) {
+        return cb(err, null);
+      }
+      return cb(null, things);
+    });
   });
 }
 export function addThingToUser(db, payload, cb) {
   const User = db.model('User');
-  return User.findById(payload.user.id, function (err, user) {
-    if (err) {
-      return cb(err, null);
+  return User.findById(payload.user.id, function (loadErr, user) {
+    if (loadErr) {
+      return cb(loadErr, null);
     }
     const ThingSchema = db.model('Thing');
     const Thing = new ThingSchema({name: payload.thing.name});
-    Thing.save(function (err2, thing) {
-      if (err2) {
-        cb(err2, null);
+    Thing.save(function (thingErr, thing) {
+      if (thingErr) {
+        cb(thingErr, null);
       } else {
         user.things.push(Thing);
-        user.save(function (err3) {
-          if (err3) {
-            cb(err3, null);
+        user.save(function (userErr) {
+          if (userErr) {
+            cb(userErr, null);
           } else {
             cb(null, thing);
           }
@@ -34,29 +41,31 @@ export function addThingToUser(db, payload, cb) {
   });
 }
 export function deleteThing(db, id, cb) {
-  const Thing = db.models.Thing;
-  Thing.forge({id: id})
-      .fetch({require: true})
-      .then(function (thing) {
-        thing.destroy().then(function () {
-          cb(null);
-        });
-      })
-      .otherwise(function (err) {
-        cb(err);
-      });
+  const Thing = db.model('Thing');
+  Thing.findByID(id, function (err, thing) {
+    if (err) {
+      cb(err);
+    }
+    thing.remove(function (deletionErr) {
+      if (deletionErr) {
+        cb(deletionErr);
+      } else {
+        cb(null);
+      }
+    });
+  });
 }
+
 export function updateThing(db, payload, cb) {
-  const Thing = db.models.Thing;
-  Thing.forge({id: payload.thingId})
-      .fetch({require: true})
-      .then(function (thing) {
-        thing.set({name: payload.thing.name}).save()
-            .then(function (savedThing) {
-              cb(null, savedThing);
-            })
-            .otherwise(function (err) {
-              cb(err, null);
-            });
+  const Thing = db.model9('Thing');
+  Thing.findByID(payload.thingId, function (err, thing) {
+    if (err) {
+      cb(err);
+    } else {
+      thing.name = payload.thing.name;
+      thing.save(function (savedThing) {
+        cb(null, savedThing);
       });
+    }
+  });
 }
