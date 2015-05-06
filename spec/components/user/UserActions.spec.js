4f0d6c2b6f99988dd25c91475285031f6861fe0c
Jussi Hallila <jussi@hallila.com>
2015-05-06 17:46:10 +0100

    Added sinon and proxyquire. Started building more tests.

---
+++ spec/components/user/UserActions.spec.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var expect = require('chai').expect;
+var sinon = require('sinon');
+var proxyquire = require('proxyquire');
+
+describe('UserActions', function () {
+  var userActions;
+  var response = {};
+  var stubDispatcher = sinon.stub();
+  before(function () {
+    var fetcherStub = sinon.stub();
+    fetcherStub.httpGet = function (url, dispatcher, context) {
+      response.url = url;
+      response.dispatcher = dispatcher;
+      response.context = context;
+    };
+    userActions = proxyquire('../../../app/components/user/UserActions',
+        {'../../services/fetcher': fetcherStub},
+        {'../../util/dispatcher': stubDispatcher}
+    );
+  });
+
+  it('should Call getData with correct params', function () {
+    var context = {};
+    userActions.getData(context);
+    expect(response.url).to.equal('/user');
+    expect(response.context).to.equal(context);
+    expect(stubDispatcher.called);
+  });
+
+});
