73b00e2efe6db8ed2879db9e72ab77d0dd06fd96
Jussi Hallila <jussi@hallila.com>
2015-05-23 09:47:16 +0200

    Update ContextMixin.js

--- app/components/common/Mixins/ContextMixin.js
+++ app/components/common/Mixins/ContextMixin.js
@@ -1,1 +1,14 @@
-//  move contextmixin here
+'use strict';
+
+import React from 'react';
+
+export default {
+  contextTypes: {
+    data: React.PropTypes.object
+  },
+  componentWillMount(){
+    if (this.context && this.context.data) {
+      this.setState(this.context.data[this.constructor.displayName]);
+    }
+  }
+};
