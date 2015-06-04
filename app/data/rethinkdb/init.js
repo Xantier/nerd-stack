a0543eada1a2c69b2e6436dc3f44af23ef1b7ff2
Jussi Hallila <jussi@hallila.com>
2015-06-04 12:17:52 +0100

    Update init.js

--- app/data/rethinkdb/init.js
+++ app/data/rethinkdb/init.js
@@ -6,7 +6,7 @@ import config from '../../config/config.json';
 let thinky = Thinky(config.rethinkdb);
 
 // Register models
-import Thing from './model/Thing'
+import Thing from './model/Thing';
 Thing.register(thinky);
 import User from './model/User';
 User.register(thinky);
