6b6ac20535241a571d317e780c908e60deb70d5a
Jussi Hallila <jussi@hallila.com>
2015-06-04 12:21:56 +0100

    Update UserActions.js

--- app/components/user/UserActions.js
+++ app/components/user/UserActions.js
@@ -3,7 +3,6 @@
 import Dispatcher from '../../util/dispatcher';
 import {httpGet} from '../../services/fetcher';
 import {UserConstants} from './UserConstants';
-import { get as getCached} from '../../util/cache';
 
 export default class UserActions {
   static getData(context) {
